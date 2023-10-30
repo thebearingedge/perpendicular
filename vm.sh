caea4e5ede8853809ef6726ac73a5ea4a3f0e279
Tim Davis <contact@timdav.is>
2023-10-29 20:01:55 -0700

    mount shared directory with proper permissions

--- vm.sh
+++ vm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -eu
 
 name="${1:-perpendicular}"
-user="${2:-$(whoami)}"
 
 vm_conf="$HOME"/.config/perpendicular/"$name".conf
 
@@ -10,19 +9,17 @@ if [ -f "$vm_conf" ]; then
   . "$vm_conf"
 fi
 
-cpus="${cpus:-4}"
+cpus="${cpus:-6}"
 memory="${memory:-8G}"
 image="${image:-jammy}"
 disk="${disk:-16G}"
 
 ansible-playbook \
   -i localhost, \
-  -e vm_user="$user" \
   -e vm_name="$name" \
   -e vm_cpus="$cpus" \
   -e vm_memory="$memory" \
   -e vm_image="$image" \
   -e vm_disk="$disk" \
-  -e host_home="$HOME" \
   -e ansible_python_interpreter="$(command -v python3 2> /dev/null)" \
   playbooks/host.yaml
